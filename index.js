ce64aa52be5c2b9b140709054e7b17f0520fb0f0
Cesar Suarez <cesar.suarez@globant.com>
2021-04-11 20:00:08 -0500

    load static resoruces

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 const http = require('http');
 const fs =require('fs');
+var path = require('path');
 
-//const { type } = require('os');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-//const timer = 300;
 
-const server = http.createServer(function(req, res){
-    console.log('funciona');
-    res.writeHead(200, {'content-type':'text/HTML' } )
-    res.write('Works');
-    res.end();
-  })
+
+
+    const server = http.createServer(function(req, res){
+      
+      var filePath = '.' + req.url;
+      var extname = path.extname(filePath);
+
+      if(req.url == '/'){
+        fileurl = 'index.html';
+      }else{
+        fileurl = './' + req.url;
+      }
+
+      switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        default:
+          contentType = 'text/HTML';
+          break;
+    }
+
+
+      fs.readFile(fileurl, function (err, html) {
+        if (err) {
+            throw err; 
+        }   
+        res.writeHead(200, {'content-type':contentType } )
+        res.write(html);
+        res.end();
+      });
+
+
+      
+    })
+
+
+    server.listen(port,hostname,()=>{
+      console.log('server is running')
+    });
+
+
+
+
   
- server.listen(port,hostname,()=>{
-   console.log('server is running')
- });
+
   
