302bb03966a8c4a8a8d190fde61a52a8fde2b313
Cesar Suarez <cesar.suarez@globant.com>
2021-04-17 17:28:09 -0500

    Save caoture image from web cam when the user click on the button

--- cam.js
+++ cam.js
@@ -28,12 +28,52 @@ function handleSuccess(stream) {
   window.stream = stream;
   video.srcObject = stream;
 }
+
+function takeASnap(){
+  const canvas = document.createElement('canvas'); // create a canvas
+  const ctx = canvas.getContext('2d'); // get its context
+  canvas.width = video.videoWidth; // set its size to the one of the video
+  canvas.height = video.videoHeight;
+  ctx.drawImage(video, 0,0); // the video
+  return new Promise((res, rej)=>{
+    canvas.toBlob(res, 'image/jpeg'); // request a Blob from the canvas
+  });
+}
+
+
+
 // Load init
 init();
 // Dibuja la imagen
 var context = canvas.getContext("2d");
 snap.addEventListener("click", function () {
   context.drawImage(video, 0, 0, 640, 480);
+  
+
+
+  //const formData = new FormData();
+ // formData.append("image", video);
+ takeASnap()
+    .then((blob)=>{
+      const data = new FormData();
+      data.append('image', blob);
+      axios.post('/uploadImage',data, {
+        headers: {
+          'Content-Type':  `multipart/form-data; boundary=${data._boundary}`
+        }
+    }).then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    });
+
+
+
+
+
+
 });
 // Reinicia la página
 clear.addEventListener("click", function () {
--- index.js
+++ index.js
@@ -1,45 +1,76 @@
 const http = require('http');
 const fs =require('fs');
 var path = require('path');
-
+const url = require('url');
+var formidable = require('formidable');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
+ function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
     const server = http.createServer(function(req, res){
-      
-      var filePath = '.' + req.url;
-      var extname = path.extname(filePath);
+      const baseURL = 'http://' + req.headers.host + '/';
+      const reqUrl = new URL(req.url,baseURL);
+      var queryData = reqUrl.query;
+      var pathName = reqUrl.pathname;
+      switch(req.method ){
+        case 'POST':
+          if(pathName == '/uploadImage'){
+            var form = new formidable.IncomingForm();
+            form.parse(req, function (err, fields, files) {
+              var oldpath = files.image.path;
+              var newpath = `./images/capture_${getRandomInt(10000,99999)}.jpg`;
+              fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+              });
+            })
+          }
+          break;
+        case 'GET':
 
-      if(req.url == '/'){
-        fileurl = 'index.html';
-      }else{
-        fileurl = './' + req.url;
+          var filePath2 = '.' + req.url;
+          var extname = path.extname(filePath2);
+          if(pathName == '/'){
+            fileurl = 'index.html';
+          }else if(pathName == '/contacto'){
+            fileurl = 'contact.html';
+          } else{
+            fileurl = './' + req.url;
+          }console.log(pathName);
+          switch (extname) {
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            default:
+              contentType = 'text/HTML';
+              break;
+        }
+
+          fs.readFile(fileurl, function (err, html) {
+            if (err) {
+                throw err; 
+            }   
+            res.writeHead(200, {'content-type':contentType } )
+            res.write(html);
+            res.end();
+          });
+        break;
       }
 
-      switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        default:
-          contentType = 'text/HTML';
-          break;
-    }
 
 
-      fs.readFile(fileurl, function (err, html) {
-        if (err) {
-            throw err; 
-        }   
-        res.writeHead(200, {'content-type':contentType } )
-        res.write(html);
-        res.end();
-      });
+      
+
 
 
       
